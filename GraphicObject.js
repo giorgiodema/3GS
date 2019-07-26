31696aa89ce0c069d6745dbf918aad4dfbe953f6
Gabriele Cervelli <15055691+polyc@users.noreply.github.com>
2019-07-26 13:00:31 +0200

    Adds library skeleton

--- GraphicObject.js
+++ GraphicObject.js
@@ -2,6 +2,36 @@
 
 class GraphicObject{
 
-    
+    constructor(vertices, normals, colors, uvCoords, colorMap, normalsMap){
+        this._vertices = vertices;
+        this._normals = normals;
+        this._colors = colors;
+        this._uvCoords = uvCoords;
+        this._colorMap = colorMap;
+        this._normalsMap = normalsMap;
+        this._children = new Array();
+        this._instanceMatrix = new mat4(); //model instantiation matrix, place the object in its relative coordinates w.r.t. father
+        this.pos = new vec3(0,0,0);
+        this.rot = new vec3(0,0,0);
+        this.scale = new vec3(1,1,1);
+    }
+
+    render(parentMatrix){
+      //var model = instanceMatrix*parentMatrix;
+      //pass model to _children
+    }
+
+    translate(){
+
+    }
+
+    rotate(){
+
+    }
+
+    scale(){
+      
+    }
+
 
 }
---
+++ Scene.js
@@ -0,0 +1,32 @@
+class Scene {
+    constructor() {
+        this.objects = new Array();
+        this.lights = new Array();
+        this.animations = new Array();
+        this.camera = new Array();
+    }
+
+    animate(){
+        //TODO
+    }
+
+    render(){
+        //TODO
+    }
+
+    addObject(){
+        //TODO
+    }
+
+    addLight(){
+        //TODO
+    }
+
+    addAnimation(){
+        //TODO
+    }
+
+    setCamera(){
+        //TODO
+    }
+}
