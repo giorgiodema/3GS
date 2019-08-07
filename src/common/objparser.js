8927181d58eae4e84d890afe92c34c672b8131b7
giorgiodema <giorgiodema@gmail.com>
2019-08-07 13:40:50 +0200

    Merge branch 'obj_parser'

--- src/common/objparser.js
+++ src/common/objparser.js
@@ -5,7 +5,13 @@ class Parser{
     constructor(serverAddr,objPath){
         this.serverAddr = serverAddr;
         this.objPath = objPath;
-        this.components = [];
+
+        this.verticesArray = [];
+        this.normalsArray = [];
+        this.uvCoordsArray = [];
+        
+        this.components = new Object();
+        this.names = [];
     }
 
     _readObjFile(){
@@ -18,31 +24,154 @@ class Parser{
             return null;
     }
 
+    _splitLine(line,sep){
+        if(line.indexOf(sep)===-1)
+            return [line];
+        
+        var sepidx = line.indexOf(sep);
+        var h = line.substring(0,sepidx);
+        var t = line.substring(sepidx+1);
+        return [h].concat(this._splitLine(t,sep));
+    }
+
+    _expandVertices(v){
+        return [v[0],v[1],v[2],v[0],v[2],v[3]];
+    }
+
     parse(){
         var s = this._readObjFile();
         while(s.length>0){
             var lineEnd = s.indexOf('\n');
-            var line = s.substr(0,lineEnd);
-            s = s.substr(lineEnd+1,s.length-1);
+            var line = s.substring(0,lineEnd-1);
+            s = s.substring(lineEnd+1);
 
-            if (!(line.startsWith("o") || line.startsWith("vt") || line.startsWith("vn") || line.startsWith("f")))
+            if (!(line.startsWith("o") || line.startsWith("v") ||line.startsWith("vt") || line.startsWith("vn") || line.startsWith("f")))
                 continue;
 
-            console.log(line);
-            console.log("---------------------");
+            if (line.startsWith("o")){
+                var name = line.substring(2);
+                this.names.push(name);
+                this.components[name] = new Object();
+                this.components[name]["vertices"] = [];
+                this.components[name]["normals"] = [];
+                this.components[name]["uvCoords"] = [];
+            }
+            else{
+                
+                var currComp = this.names[this.names.length-1];
+
+                if(line.startsWith("vt")){
+                    var lines = this._splitLine(line.substring(3),' ');
+                    var u = parseFloat(lines[0]);
+                    var v = parseFloat(lines[1]);
+                    this.uvCoordsArray.push(vec2(u,v));
+                }
+    
+                else if(line.startsWith("vn")){
+                    var lines = this._splitLine(line.substring(3),' ');
+                    var x = parseFloat(lines[0]);
+                    var y = parseFloat(lines[1]);
+                    var z = parseFloat(lines[2]);
+                    this.normalsArray.push(vec3(x,y,z));
+                }
+    
+                else if(line.startsWith("v")){
+                    var lines = this._splitLine(line.substring(2),' ');
+                    var x = parseFloat(lines[0]);
+                    var y = parseFloat(lines[1]);
+                    var z = parseFloat(lines[2]);
+                    this.verticesArray.push(vec3(x,y,z));
+                }
+    
+    
+                else if(line.startsWith("f")){
+                    var lines = this._splitLine(line.substring(2),' ');
+    
+                    var vidxArray = [];
+                    var nidxArray = [];
+                    var uvidxArray = [];
+
+                    lines.forEach(l => {
+                        // vertex \\ normal (no texture)
+                        if(l.indexOf('//')>=0){
+                            var a = this._splitLine(l,"//");
+                            vidxArray.push(parseInt(a[0]));
+                            nidxArray.push(parseInt(a[1]));
+                        }
+                        else{
+                            var a = this._splitLine(l,"/");
+                            // vertex / uvcoord / normal
+                            if(a.length===3){
+                                vidxArray.push(parseInt(a[0]));
+                                uvidxArray.push(parseInt(a[1]));
+                                nidxArray.push(parseInt(a[2]));
+                            }
+                            // vertex / uvcoord
+                            else if(a.length===2){
+                                vidxArray.push(parseInt(a[0]));
+                                uvidxArray.push(parseInt(a[1]));
+                            }
+                        }
+                    });
+
+                    // triangles
+                    if(vidxArray.length===3){
+                        vidxArray.forEach(vidx => {
+                            this.components[currComp]["vertices"].push(this.verticesArray[vidx]);
+                        });
+                        nidxArray.forEach(nidx => {
+                            this.components[currComp]["normals"].push(this.normalsArray[nidx]);
+                        });
+                        uvidxArray.forEach(uvidx => {
+                            this.components[currComp]["uvCoords"].push(this.uvCoordsArray[uvidx]);
+                        });
+                    }
+
+                    // quadrilaterals
+                    else if(vidxArray.length==4){
+                        var vidxArrayExp = this._expandVertices(vidxArray);
+                        var nidxArrayExp = nidxArray.length === 0 ? [] : this._expandVertices(nidxArray);
+                        var uvidxArrayExp = uvidxArray.length === 0 ? [] : this._expandVertices(uvidxArray);
+                        vidxArrayExp.forEach(vidx => {
+                            this.components[currComp]["vertices"].push(this.verticesArray[vidx]);
+                        });
+                        nidxArrayExp.forEach(nidx => {
+                            this.components[currComp]["normals"].push(this.normalsArray[nidx]);
+                        });
+                        uvidxArrayExp.forEach(uvidx => {
+                            this.components[currComp]["uvCoords"].push(this.uvCoordsArray[uvidx]);
+                        });
+                    }
+    
+                }
+            }
+
         }
     }
 
-    getVertices(){
-        return this.vertices;
+    getComponents(){
+        return this.names.slice(0);
     }
 
-    getNormals(){
-        return this.normals;
+    getVertices(componentName){
+        if(this.names.findIndex(componentName))
+            return this.components[componentName]["vertices"];
+        else
+            return null;
     }
 
-    getUVCoords(){
-        return this.uvcoords;
+    getNormals(componentName){
+        if(this.names.findIndex(componentName))
+            return this.components[componentName]["normals"];
+        else
+            return null;
+    }
+
+    getUVCoords(componentName){
+        if(this.names.findIndex(componentName))
+            return this.components[componentName]["uvCoords"];
+        else
+            return null;
     }
     
 }
