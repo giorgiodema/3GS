7a2ebeaceeefb65903e738093e22193e42e66d71
polyc <ichigo69600@gmail.com>
2019-08-19 20:29:47 +0200

    implements _animateScene(), renderScene(), Adder functions, setActiveCamera()

--- src/lib/Scene.js
+++ src/lib/Scene.js
@@ -1,38 +1,78 @@
 class Scene {
+
+    this._activeCamera;
+
     constructor() {
-        this.objects = new Array();
-        this.lights = new Array();
-        this.animations = new Array();
-        this.camera = new Array();
+        this._objects = new Array();
+        this._lights = new Array();
+        this._animations = new Array();
+        this._cameras = new Array();
     }
 
-    // Gets called inside render, calls all animations
-    animate(){
-        //TODO
+    // Gets called inside renderScene(), calls animate() function for all GraphicObjects
+    _animateScene(){
+        var len = this._animations.length;
+        for(var i = 0; i < len i++){
+
+            if(this._animations[i] != NULL || !(this._animations[i].length != 0)
+                //Processes an Animation object
+                this._animations[i].animate();
+
+        }
     }
 
-    // Calls animate, then all renders of children objects
-    render(){
-        //TODO
+    // Calls animateScene(), then renders all GraphicObjects
+    renderScene(){
+
+        if(this._animations != NULL || !(this._animations.length != 0)
+            //Updates scene objects animation parameters
+            _animateScene();
+
+
+        var len = this._objects.length;
+
+        if(this._objects != NULL || !(len != 0){
+
+            for(var i = 0; i < len; i++){
+                if(this._objects[i] != NULL)
+                    //renders a GraphicObject
+                    this._objects[i].render();
+            }
+
+        }
+
     }
 
-    addObject(){
-        //TODO
+//******************************** ADD METHODS *********************************
+    addObject(object){
+        if(object != NULL) this._objects.push(object);
     }
 
-    addLight(){
-        //TODO
+    addLight(light){
+        if(light != NULL) this._lights.push(light);
     }
 
-    addAnimation(){
-        //TODO
+    addAnimation(animation){
+        if(animation != NULL) this._animations.push(animation);
     }
 
-    addCamera(){
+    addCamera(camera){
+        if(object != NULL) this._cameras.push(camera);
+    }
+
+//******************************************************************************
+
+    //Makes a scene camera the working scene camera
+    setActiveCamera(cameraIndex){
+
+        if (cameraIndex >= 0 && cameraIndex < this._cameras.length){
+
+            console.assert(this._cameras[cameraIndex] != NULL,
+                    "[setActiveCamera] Undefined camera selected");
+
+            this._activeCamera = this._cameras[cameraIndex];
+        }
 
     }
 
-    setActiveCamera(){
-        //TODO
-    }
 }
