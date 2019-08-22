ea1a6ea20262a00d1636cd2a96d48e7fc29eec68
Sergio Pedri <sergio0694@live.com>
2019-08-22 17:46:51 +0200

    Added callbacks for the arrow keys

--- src/lib/InputController.js
+++ src/lib/InputController.js
@@ -5,10 +5,17 @@
 export default class InputController {
     constructor() {
 
+        // Arrow keys states
         this._isUpArrowKeyDown = false;
         this._isDownArrowKeyDown = false;
         this._isLeftArrowKeyDown = false;
         this._isRightArrowKeyDown = false;
+
+        // Arrow keys handlers
+        this._onUpArrowKeyDown = null;
+        this._onDownArrowKeyDown = null;
+        this._onLeftArrowKeyDown = null;
+        this._onRightArrowKeyDown = null;
 
         window.onkeydown = e => this._trackArrowKey(e, true);
         window.onkeyup = e => this._trackArrowKey(e, false);
@@ -21,25 +28,73 @@ export default class InputController {
 
     // Gets whether or not the down arrow key is currently down
     get isDownArrowKeyDown() {
-        return this.isDownArrowKeyDown;
+        return this._isDownArrowKeyDown;
     }
 
     // Gets whether or not the left arrow key is currently down
     get isLeftArrowKeyDown() {
-        return this.isLeftArrowKeyDown;
+        return this._isLeftArrowKeyDown;
     }
 
     // Gets whether or not the right arrow key is currently down
     get isRightArrowKeyDown() {
-        return this.isRightArrowKeyDown;
+        return this._isRightArrowKeyDown;
     }
 
+    // Sets the callback to invoke when the up arrow key is pressed
+    get onUpArrowKeyDown() {
+        return this._onUpArrowKeyDown;
+    }
+
+    // Sets the callback to invoke when the down arrow key is pressed
+    get onDownArrowKeyDown() {
+        return this._onDownArrowKeyDown;
+    }
+
+    // Sets the callback to invoke when the left arrow key is pressed
+    get onLeftArrowKeyDown() {
+        return this._onLeftArrowKeyDown;
+    }
+
+    // Sets the callback to invoke when the right arrow key is pressed
+    get onRightArrowKeyDown() {
+        return this._onRightArrowKeyDown;
+    }
+
+    // Tracks when an arrow key is being pressed
     _trackArrowKey(e, state) {
         e = e || window.event;
 
-        if (e.keyCode == '38') this._isUpArrowKeyDown = state;
-        else if (e.keyCode == '40') this._isDownArrowKeyDown = state;
-        else if (e.keyCode == '37') this._isLeftArrowKeyDown = state;
-        else if (e.keyCode == '39') this._isRightArrowKeyDown = state;
+        // Up
+        if (e.keyCode == '38') {
+            this._isUpArrowKeyDown = state;
+            if (typeof this._onUpArrowKeyDown === 'function') {
+                this._onUpArrowKeyDown();
+            }
+        }
+        else if (e.keyCode == '40') {
+
+            // Down
+            this._isDownArrowKeyDown = state;
+            if (typeof this._onDownArrowKeyDown === 'function') {
+                this._onDownArrowKeyDown();
+            }
+        }
+        else if (e.keyCode == '37') {
+
+            // Left
+            this._isLeftArrowKeyDown = state;
+            if (typeof this._onLeftArrowKeyDown === 'function') {
+                this._onLeftArrowKeyDown();
+            }
+        }
+        else if (e.keyCode == '39') {
+
+            // Right
+            this._isRightArrowKeyDown = state;
+            if (typeof this._onRightArrowKeyDown === 'function') {
+                this._onRightArrowKeyDown();
+            }
+        }
     }
 }
