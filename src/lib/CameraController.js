4fbc2eec56954bc2da7abb92ace8a9a8794c34d4
giorgiodema <giorgiodema@gmail.com>
2019-08-26 16:36:04 +0200

    zoom

--- src/lib/CameraController.js
+++ src/lib/CameraController.js
@@ -2,24 +2,38 @@ class CameraController{
     constructor(camera){
         this.camera = camera;
         this.step = 0.05;
+        this.zoom = 1;
 
         // bound object controller
         this._objectController = null;
         this._distance = 0;
         this._height = 0;
         document.addEventListener('mousemove',this._mouseController(this));
+        document.addEventListener('wheel',this._wheelController(this));
     }
 
     _mouseController(contr){
         return function(e){
             //console.log("X:"+e.movementX);
             //console.log("Y:"+e.movementY);
+            if(contr._objectController!=null && (contr._objectController.moving || contr._objectController.rotatingLeft || contr._objectController.rotatingRight))
+                return;
             let sx = contr.step * e.movementX;
             let sy = contr.step * e.movementY;
-            contr.camera.at = vec3(contr.camera.at[0] - sx,contr.camera.at[1] - sy,contr.camera.at[2]);
+            contr.camera.at = vec3(contr.camera.at[0] + sx,contr.camera.at[1] - sy,contr.camera.at[2]);
             //console.log(contr.camera.at);
         }
    
+    }
+
+    _wheelController(contr){
+        return function(e){
+            if(e.deltaY < 0)
+                contr._distance-=contr.zoom;
+            else
+                contr._distance+=contr.zoom;
+            contr.camera.eye = vec3(contr.camera.eye[0],contr._distance,contr.camera.eye[2]);
+        }
     }
 
     bindObjectController(objController,distance,height){
