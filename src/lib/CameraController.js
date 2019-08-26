3ff2148c070b12290cbe24c3e4be38e0d1a250b2
giorgiodema <giorgiodema@gmail.com>
2019-08-26 17:00:50 +0200

    refactor

--- src/lib/CameraController.js
+++ src/lib/CameraController.js
@@ -6,8 +6,6 @@ class CameraController{
 
         // bound object controller
         this._objectController = null;
-        this._distance = 0;
-        this._height = 0;
         document.addEventListener('mousemove',this._mouseController(this));
         document.addEventListener('wheel',this._wheelController(this));
     }
@@ -18,7 +16,8 @@ class CameraController{
                 return;
             let sx = contr._step * e.movementX;
             let sy = contr._step * e.movementY;
-            contr._camera.at = vec3(contr._camera.at[0] + sx,contr._camera.at[1] - sy,contr._camera.at[2]);
+            contr._camera.at[0] += sx;
+            contr._camera.at[1] -= sy;
         }
    
     }
@@ -26,16 +25,13 @@ class CameraController{
     _wheelController(contr){
         return function(e){
             if(e.deltaY < 0)
-                contr._distance-=contr._zoom;
+            contr._camera.eye[1]-=contr._zoom;
             else
-                contr._distance+=contr._zoom;
-            contr._camera.eye = vec3(contr._camera.eye[0],contr._distance,contr._camera.eye[2]);
+            contr._camera.eye[1]+=contr._zoom;
         }
     }
 
     bindObjectController(objController,distance,height){
-        this._distance = distance;
-        this._height = height;
         this._objectController = objController;
         this._camera.eye = vec3(this._camera.eye[0],height,distance);
         this._camera.at = objController.object.pos;
