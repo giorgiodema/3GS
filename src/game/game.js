6afd810e591cb09576c985d9d66d47d33c5900bf
giorgiodema <giorgiodema@gmail.com>
2019-08-27 16:03:10 +0200

    add character

--- src/game/game.js
+++ src/game/game.js
@@ -11,6 +11,7 @@ window.onload = () =>
 
         
         let maze = buildMazeGeometry();
+        let character = buildCharacterGeometry();
 
         let camera = new PerspectiveCamera();
 
@@ -53,3 +54,62 @@ function buildMazeGeometry(){
 
     return tree;
 }
+
+function buildCharacterGeometry(){
+    let objparser = new Parser("http://localhost:9000", "src/game/Assets/Character/model_separated.obj");
+    objparser.parse();
+    // initialize character
+    let colors = new Array(objparser.getVertices("Torso").length).fill(new vec3(100/255,90/255,68/255));
+    let torso = new GraphicObject(objparser.getVertices("Torso"), objparser.getNormals("Torso"), colors);
+
+    colors = new Array(objparser.getVertices("Head").length).fill(new vec3(100/255,90/255,68/255));
+    let head = new GraphicObject(objparser.getVertices("Head"), objparser.getNormals("Head"), colors);
+
+    colors = new Array(objparser.getVertices("Upper_Leg_R").length).fill(new vec3(100/255,90/255,68/255));
+    let upperLegR = new GraphicObject(objparser.getVertices("Upper_Leg_R"), objparser.getNormals("Upper_Leg_R"), colors);
+
+    colors = new Array(objparser.getVertices("Upper_Leg_L").length).fill(new vec3(100/255,90/255,68/255));
+    let upperLegL = new GraphicObject(objparser.getVertices("Upper_Leg_L"), objparser.getNormals("Upper_Leg_L"), colors);
+
+    colors = new Array(objparser.getVertices("Upper_Arm_R").length).fill(new vec3(100/255,90/255,68/255));
+    let upperArmR = new GraphicObject(objparser.getVertices("Upper_Arm_R"), objparser.getNormals("Upper_Arm_R"), colors);
+
+    colors = new Array(objparser.getVertices("Upper_Arm_L").length).fill(new vec3(100/255,90/255,68/255));
+    let upperArmL = new GraphicObject(objparser.getVertices("Upper_Arm_L"), objparser.getNormals("Upper_Arm_L"), colors);
+
+    colors = new Array(objparser.getVertices("Lower_Arm_R").length).fill(new vec3(100/255,90/255,68/255));
+    let lowerArmR = new GraphicObject(objparser.getVertices("Lower_Arm_R"), objparser.getNormals("Lower_Arm_R"), colors);
+
+    colors = new Array(objparser.getVertices("Lower_Arm_L").length).fill(new vec3(100/255,90/255,68/255));
+    let lowerArmL = new GraphicObject(objparser.getVertices("Lower_Arm_L"), objparser.getNormals("Lower_Arm_L"), colors);
+
+    colors = new Array(objparser.getVertices("Lower_Leg_R").length).fill(new vec3(100/255,90/255,68/255));
+    let lowerLegR = new GraphicObject(objparser.getVertices("Lower_Leg_R"), objparser.getNormals("Lower_Leg_R"), colors);
+
+    colors = new Array(objparser.getVertices("Lower_Leg_L").length).fill(new vec3(100/255,90/255,68/255));
+    let lowerLegL = new GraphicObject(objparser.getVertices("Lower_Leg_L"), objparser.getNormals("Lower_Leg_L"), colors);
+
+    colors = new Array(objparser.getVertices("Foot_R").length).fill(new vec3(100/255,90/255,68/255));
+    let footR = new GraphicObject(objparser.getVertices("Foot_R"), objparser.getNormals("Foot_R"), colors);
+
+    colors = new Array(objparser.getVertices("Foot_L").length).fill(new vec3(100/255,90/255,68/255));
+    let footL = new GraphicObject(objparser.getVertices("Foot_L"), objparser.getNormals("Foot_L"), colors);
+
+    scene.addObject(torso);
+    torso.addChild(head);
+    torso.addChild(upperLegR);
+    torso.addChild(upperLegL);
+    torso.addChild(upperArmR);
+    torso.addChild(upperArmL);
+    upperArmR.addChild(lowerArmR);
+    upperArmL.addChild(lowerArmL);
+    upperLegR.addChild(lowerLegR);
+    upperLegL.addChild(lowerLegL);
+    lowerLegR.addChild(footR);
+    lowerLegL.addChild(footL);
+
+    
+
+    return torso;
+}
+
