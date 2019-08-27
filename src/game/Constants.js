993e57e3be9d8afaaaa47c2a9c4829c10bd39c40
giorgiodema <giorgiodema@gmail.com>
2019-08-27 11:53:39 +0200

    add constants, code refactoring

---
+++ src/game/Constants.js
@@ -0,0 +1,36 @@
+// An enum to keep track of the cells state
+const _CELL = {
+    EMPTY: 0,
+    UNDEFINED: 1,
+    WALL: 2,
+    PASSAGE: 3,
+    PLAYER: 4,
+    TARGET: 5
+}
+
+// An enum that indicates a movement direction
+const _DIRECTION = {
+    UP: 0,
+    DOWN: 1,
+    LEFT: 2,
+    RIGHT: 3
+}
+
+class Constants {
+
+    static get GRID_WIDTH() {
+        return 20;
+    }
+
+    static get GRID_HEIGHT() {
+        return 20;
+    }
+
+    static get CELL() {
+        return _CELL;
+    }
+
+    static get DIRECTION() {
+        return _DIRECTION;
+    }
+}
--- src/game/game.js
+++ src/game/game.js
@@ -6,30 +6,9 @@ window.onload = function init()
     scene = new Scene("gl-canvas");
     scene.init(function() {
 
-
-
-        // initialize colored cubes, to test camera and object controllers
-
-        for(let i = 0; i < 500; i++){
-            let ci = new Cube();
-            let scale = Math.floor(Math.random()*5);
-            ci.scale(scale,scale,scale);
-
-            let x = Math.floor(Math.random()*100);
-            let y = Math.floor(Math.random()*100);
-            let z = Math.floor(Math.random()*100);
-
-            x = Math.random() >= 0.5 ? x : -x;
-            y = Math.random() >= 0.5 ? y : -y;
-            z = Math.random() >= 0.5 ? z : -z;
-            
-            ci.setPosition(x,y,z);
-
-            ci.setRotation(Math.floor(Math.random()*360),[1,0,0]);
-            ci.setRotation(Math.floor(Math.random()*360),[0,1,0]);
-            ci.setRotation(Math.floor(Math.random()*360),[0,0,1]);
-            scene.addObject(ci);
-        }
+        var maze = new Maze(Constants.GRID_WIDTH,Constants.GRID_HEIGHT);
+        var g = maze.grid;
+        var c = Constants.CELL.PLAYER;
 
         let camera = new PerspectiveCamera();
 
