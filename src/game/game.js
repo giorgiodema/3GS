292299761ddc343c250385aef8fd4ebda7113c0e
giorgiodema <giorgiodema@gmail.com>
2019-08-27 11:32:51 +0200

    Cube

--- src/game/game.js
+++ src/game/game.js
@@ -5,69 +5,13 @@ window.onload = function init()
 {
     scene = new Scene("gl-canvas");
     scene.init(function() {
-        let objparser = new Parser("http://localhost:9000", "src/game/Assets/Character/model_separated.obj");
-        objparser.parse();
 
-        console.log(objparser.getComponents());
 
-        // initialize character
-        let colors = new Array(objparser.getVertices("Torso").length).fill(new vec3(100/255,90/255,68/255));
-        let torso = new GraphicObject(objparser.getVertices("Torso"), objparser.getNormals("Torso"), colors);
-
-        colors = new Array(objparser.getVertices("Head").length).fill(new vec3(100/255,90/255,68/255));
-        let head = new GraphicObject(objparser.getVertices("Head"), objparser.getNormals("Head"), colors);
-
-        colors = new Array(objparser.getVertices("Upper_Leg_R").length).fill(new vec3(100/255,90/255,68/255));
-        let upperLegR = new GraphicObject(objparser.getVertices("Upper_Leg_R"), objparser.getNormals("Upper_Leg_R"), colors);
-
-        colors = new Array(objparser.getVertices("Upper_Leg_L").length).fill(new vec3(100/255,90/255,68/255));
-        let upperLegL = new GraphicObject(objparser.getVertices("Upper_Leg_L"), objparser.getNormals("Upper_Leg_L"), colors);
-
-        colors = new Array(objparser.getVertices("Upper_Arm_R").length).fill(new vec3(100/255,90/255,68/255));
-        let upperArmR = new GraphicObject(objparser.getVertices("Upper_Arm_R"), objparser.getNormals("Upper_Arm_R"), colors);
-
-        colors = new Array(objparser.getVertices("Upper_Arm_L").length).fill(new vec3(100/255,90/255,68/255));
-        let upperArmL = new GraphicObject(objparser.getVertices("Upper_Arm_L"), objparser.getNormals("Upper_Arm_L"), colors);
-
-        colors = new Array(objparser.getVertices("Lower_Arm_R").length).fill(new vec3(100/255,90/255,68/255));
-        let lowerArmR = new GraphicObject(objparser.getVertices("Lower_Arm_R"), objparser.getNormals("Lower_Arm_R"), colors);
-
-        colors = new Array(objparser.getVertices("Lower_Arm_L").length).fill(new vec3(100/255,90/255,68/255));
-        let lowerArmL = new GraphicObject(objparser.getVertices("Lower_Arm_L"), objparser.getNormals("Lower_Arm_L"), colors);
-
-        colors = new Array(objparser.getVertices("Lower_Leg_R").length).fill(new vec3(100/255,90/255,68/255));
-        let lowerLegR = new GraphicObject(objparser.getVertices("Lower_Leg_R"), objparser.getNormals("Lower_Leg_R"), colors);
-
-        colors = new Array(objparser.getVertices("Lower_Leg_L").length).fill(new vec3(100/255,90/255,68/255));
-        let lowerLegL = new GraphicObject(objparser.getVertices("Lower_Leg_L"), objparser.getNormals("Lower_Leg_L"), colors);
-
-        colors = new Array(objparser.getVertices("Foot_R").length).fill(new vec3(100/255,90/255,68/255));
-        let footR = new GraphicObject(objparser.getVertices("Foot_R"), objparser.getNormals("Foot_R"), colors);
-
-        colors = new Array(objparser.getVertices("Foot_L").length).fill(new vec3(100/255,90/255,68/255));
-        let footL = new GraphicObject(objparser.getVertices("Foot_L"), objparser.getNormals("Foot_L"), colors);
-
-        scene.addObject(torso);
-        torso.addChild(head);
-        torso.addChild(upperLegR);
-        torso.addChild(upperLegL);
-        torso.addChild(upperArmR);
-        torso.addChild(upperArmL);
-        upperArmR.addChild(lowerArmR);
-        upperArmL.addChild(lowerArmL);
-        upperLegR.addChild(lowerLegR);
-        upperLegL.addChild(lowerLegL);
-        lowerLegR.addChild(footR);
-        lowerLegL.addChild(footL);
-
-        torso.rotate(270.0, [0, 1, 0]);
-        let charController = new ObjectController(torso);
 
         // initialize colored cubes, to test camera and object controllers
-        let c = new CubeV();
-        let cubes = new Array();
+
         for(let i = 0; i < 500; i++){
-            let ci = new GraphicObject(c.getVertices(),[],new Array(c.getVertices().length).fill(vec3(Math.random(),Math.random(),Math.random())));
+            let ci = new Cube();
             let scale = Math.floor(Math.random()*5);
             ci.scale(scale,scale,scale);
 
@@ -96,7 +40,6 @@ window.onload = function init()
         camera.setFar(1000);
 
         let cc = new CameraController(camera);
-        cc.bindObjectController(charController,30,30);
 
         render();
     });
--- src/lib/Cube.js
+++ src/lib/Cube.js
@@ -1,7 +1,7 @@
 
-class CubeV{
+class Cube extends GraphicObject{
     constructor(){
-        this._cubeVertices = [
+        var _cubeVertices = [
             vec3( -0.5, -0.5,  0.5),
             vec3( -0.5,  0.5,  0.5),
             vec3( 0.5,  0.5,  0.5),
@@ -12,28 +12,83 @@ class CubeV{
             vec3( 0.5, -0.5, -0.5)
         ];
 
-        this.pointsArray = new Array();
+        var _texCoord = [
+            vec2(0, 0),
+            vec2(0, 1),
+            vec2(1, 1),
+            vec2(1, 0)
+        ];
 
-    }
+        var _pointsArray = new Array();
+        var _normalsArray = new Array();
+        var _texCoordArray = new Array();
+        var _colorsArray = new Array(36).fill(vec3(0.0,0.0,0.0));
 
-    pushQuadVertices(a, b, c, d) {
-        this.pointsArray.push(this._cubeVertices[a]);
-        this.pointsArray.push(this._cubeVertices[b]);
-        this.pointsArray.push(this._cubeVertices[c]);
-        this.pointsArray.push(this._cubeVertices[a]);
-        this.pointsArray.push(this._cubeVertices[c]);
-        this.pointsArray.push(this._cubeVertices[d]);
-    }
+        var pushQuadVertices = function(a, b, c, d) {
+            _pointsArray.push(_cubeVertices[a]);
+            _texCoordArray.push(_texCoord[0]);
+            _pointsArray.push(_cubeVertices[b]);
+            _texCoordArray.push(_texCoord[1]);
+            _pointsArray.push(_cubeVertices[c]);
+            _texCoordArray.push(_texCoord[2]);
+            _pointsArray.push(_cubeVertices[a]);
+            _texCoordArray.push(_texCoord[0]);
+            _pointsArray.push(_cubeVertices[c]);
+            _texCoordArray.push(_texCoord[2]);
+            _pointsArray.push(_cubeVertices[d]);
+            _texCoordArray.push(_texCoord[3]);
+        };
 
-    getVertices(){
-        if (this.pointsArray.length===0){
-            this.pushQuadVertices( 1, 0, 3, 2);
-            this.pushQuadVertices( 2, 3, 7, 6);
-            this.pushQuadVertices( 3, 0, 4, 7);
-            this.pushQuadVertices( 6, 5, 1, 2);
-            this.pushQuadVertices( 4, 5, 6, 7);
-            this.pushQuadVertices( 5, 4, 0, 1);
-        }
-    return this.pointsArray.slice();
+        pushQuadVertices( 1, 0, 3, 2);
+        pushQuadVertices( 2, 3, 7, 6);
+        pushQuadVertices( 3, 0, 4, 7);
+        pushQuadVertices( 6, 5, 1, 2);
+        pushQuadVertices( 4, 5, 6, 7);
+        pushQuadVertices( 5, 4, 0, 1);
+
+        // Face ABCD
+        var v1 = subtract(_cubeVertices[1],_cubeVertices[0]);
+        var v2 = subtract(_cubeVertices[2],_cubeVertices[1]); 
+        var n1 = cross(v2, v1);
+        n1 = vec4(n1[0],n1[1],n1[2],0);
+
+        // Face DCGH
+        v1 = subtract(_cubeVertices[2],_cubeVertices[3]);
+        v2 = subtract(_cubeVertices[6],_cubeVertices[2]);
+        var n2 = cross(v2, v1);
+        n2 = vec4(n2[0],n2[1],n2[2],0);
+
+        // Face ADHE
+        v1 = subtract(_cubeVertices[3],_cubeVertices[0]);
+        v2 = subtract(_cubeVertices[7],_cubeVertices[3]);
+        var n3 = cross(v2, v1);
+        n3 = vec4(n3[0],n3[1],n3[2],0);
+
+        // Face FGCB
+        v1 = subtract(_cubeVertices[6],_cubeVertices[5]);
+        v2 = subtract(_cubeVertices[2],_cubeVertices[6]);
+        var n4 = cross(v2, v1);
+        n4 = vec4(n4[0],n4[1],n4[2],0);
+
+        // Face FEHG
+        v1 = subtract(_cubeVertices[4],_cubeVertices[5]);
+        v2 = subtract(_cubeVertices[7],_cubeVertices[4]);
+        var n5 = cross(v2, v1);
+        n5 = vec4(n5[0],n5[1],n5[2],0);
+
+        // Face EFBA
+        v1 = subtract(_cubeVertices[5],_cubeVertices[4]);
+        v2 = subtract(_cubeVertices[1],_cubeVertices[5]);
+        var n6 = cross(v2, v1);
+        n6 = vec4(n6[0],n6[1],n6[2],0);
+
+        _normalsArray.push(n1);_normalsArray.push(n1);_normalsArray.push(n1);_normalsArray.push(n1);_normalsArray.push(n1);_normalsArray.push(n1);
+        _normalsArray.push(n2);_normalsArray.push(n2);_normalsArray.push(n2);_normalsArray.push(n2);_normalsArray.push(n2);_normalsArray.push(n2);
+        _normalsArray.push(n3);_normalsArray.push(n3);_normalsArray.push(n3);_normalsArray.push(n3);_normalsArray.push(n3);_normalsArray.push(n3);
+        _normalsArray.push(n4);_normalsArray.push(n4);_normalsArray.push(n4);_normalsArray.push(n4);_normalsArray.push(n4);_normalsArray.push(n4);
+        _normalsArray.push(n5);_normalsArray.push(n5);_normalsArray.push(n5);_normalsArray.push(n5);_normalsArray.push(n5);_normalsArray.push(n5);
+        _normalsArray.push(n6);_normalsArray.push(n6);_normalsArray.push(n6);_normalsArray.push(n6);_normalsArray.push(n6);_normalsArray.push(n6);
+
+        super(_pointsArray,_normalsArray, _colorsArray, _texCoordArray, null, null);
     }
 }      
