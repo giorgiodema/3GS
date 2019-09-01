f7fddcf40319ca76342daf246648838845ad5eb8
giorgiodema <giorgiodema@gmail.com>
2019-09-01 16:59:07 +0200

    update normals

--- src/lib/Cube.js
+++ src/lib/Cube.js
@@ -39,47 +39,47 @@ class Cube extends GraphicObject{
             _texCoordArray.push(_texCoord[3]);
         };
 
-        pushQuadVertices( 1, 0, 3, 2);
-        pushQuadVertices( 2, 3, 7, 6);
-        pushQuadVertices( 3, 0, 4, 7);
-        pushQuadVertices( 6, 5, 1, 2);
-        pushQuadVertices( 4, 5, 6, 7);
-        pushQuadVertices( 5, 4, 0, 1);
+        pushQuadVertices( 0, 1, 2, 3); // FRONT
+        pushQuadVertices( 4, 5, 6, 7); // BACK
+        pushQuadVertices( 0, 4, 7, 3); // BOTTOM
+        pushQuadVertices( 1, 5, 6, 2); // UP
+        pushQuadVertices( 0, 1, 5, 4); // LEFT 
+        pushQuadVertices( 3, 2, 6, 7); // RIGHT
 
-        // Face ABCD
-        var v1 = subtract(_cubeVertices[1],_cubeVertices[0]);
+        // FRONT
+        var v1 = subtract(_cubeVertices[0],_cubeVertices[1]);
         var v2 = subtract(_cubeVertices[2],_cubeVertices[1]); 
-        var n1 = vec4(normalize(cross(v2, v1)));
+        var n1 = vec4(normalize(cross(v1, v2)));
         n1 = vec4(n1[0],n1[1],n1[2],1.0);
 
-        // Face DCGH
-        v1 = subtract(_cubeVertices[2],_cubeVertices[3]);
-        v2 = subtract(_cubeVertices[6],_cubeVertices[2]);
-        var n2 = vec4(normalize(cross(v2, v1)));
+        // BACK
+        v1 = subtract(_cubeVertices[5],_cubeVertices[4]);
+        v2 = subtract(_cubeVertices[7],_cubeVertices[4]);
+        var n2 = vec4(normalize(cross(v1, v2)));
         n2 = vec4(n2[0],n2[1],n2[2],1.0);
 
-        // Face ADHE
-        v1 = subtract(_cubeVertices[3],_cubeVertices[0]);
-        v2 = subtract(_cubeVertices[7],_cubeVertices[3]);
-        var n3 = vec4(normalize(cross(v2, v1)));
+        // BOTTOM
+        v1 = subtract(_cubeVertices[4],_cubeVertices[0]);
+        v2 = subtract(_cubeVertices[3],_cubeVertices[0]);
+        var n3 = vec4(normalize(cross(v1, v2)));
         n3 = vec4(n3[0],n3[1],n3[2],1.0);
 
-        // Face FGCB
-        v1 = subtract(_cubeVertices[6],_cubeVertices[5]);
-        v2 = subtract(_cubeVertices[2],_cubeVertices[6]);
-        var n4 = vec4(normalize(cross(v2, v1)));
+        // UP
+        v1 = subtract(_cubeVertices[2],_cubeVertices[1]);
+        v2 = subtract(_cubeVertices[5],_cubeVertices[1]);
+        var n4 = vec4(normalize(cross(v1, v2)));
         n4 = vec4(n4[0],n4[1],n4[2],1.0);
 
-        // Face FEHG
-        v1 = subtract(_cubeVertices[4],_cubeVertices[5]);
-        v2 = subtract(_cubeVertices[7],_cubeVertices[4]);
-        var n5 = vec4(normalize(cross(v2, v1)));
+        // LEFT
+        v1 = subtract(_cubeVertices[1],_cubeVertices[0]);
+        v2 = subtract(_cubeVertices[4],_cubeVertices[0]);
+        var n5 = vec4(normalize(cross(v1, v2)));
         n5 = vec4(n5[0],n5[1],n5[2],1.0);
 
-        // Face EFBA
-        v1 = subtract(_cubeVertices[5],_cubeVertices[4]);
-        v2 = subtract(_cubeVertices[1],_cubeVertices[5]);
-        var n6 = vec4(normalize(cross(v2, v1)));
+        // RIGHT
+        v1 = subtract(_cubeVertices[7],_cubeVertices[3]);
+        v2 = subtract(_cubeVertices[2],_cubeVertices[3]);
+        var n6 = vec4(normalize(cross(v1, v2)));
         n6 = vec4(n6[0],n6[1],n6[2],1.0);
 
         _normalsArray.push(n1);_normalsArray.push(n1);_normalsArray.push(n1);_normalsArray.push(n1);_normalsArray.push(n1);_normalsArray.push(n1);
